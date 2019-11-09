0f2728802120bf814d53091e062df39361ee6dbc
Samantha Sample <ssample812@gmail.com>
2019-11-08 22:58:52 -0600

    Add components for login

--- communityCanvas/src/app/app.module.ts
+++ communityCanvas/src/app/app.module.ts
@@ -9,29 +9,23 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuth } from '@angular/fire/auth';
 
 import { environment } from '../environments/environment';
-<<<<<<< HEAD
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 import { PrivatePageComponent } from './components/private-page/private-page.component';
-=======
 import { UploadFileComponent } from './components/upload-file/upload-file.component';
 import { UploadTaskComponent } from './components/upload-task/upload-task.component';
 import { DropzoneDirective } from './directives/dropzone.directive';
->>>>>>> 88b38c7ddc65f45b1ef04e1f4f55283612dc0ca4
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     LoginPageComponent,
     RegisterPageComponent,
     PrivatePageComponent
-=======
     UploadFileComponent,
     UploadTaskComponent,
     DropzoneDirective
->>>>>>> 88b38c7ddc65f45b1ef04e1f4f55283612dc0ca4
   ],
   imports: [
     BrowserModule,

5edb098de809efa619c9374df0ffbe0b520563b0
Rebecca Parker <rfpparker@gmail.com>
2019-11-09 15:08:18 -0600

    modified profile

--- communityCanvas/src/app/components/profile-page/profile-page.component.ts
+++ communityCanvas/src/app/components/profile-page/profile-page.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from 'src/app/services/profile.service';
+import { Users } from 'src/app/interfaces/users';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.css']
 })
-export class ProfilePageComponent implements OnInit {
-  private username: string
-  private bio: string
-  private image: File
-  private mediums: Array<string>
-  private profileService = new ProfileService(this.username);
+export class ProfilePageComponent implements Users {
+  public password: string;
+  public username: string;
+  public bio: string;
+  public image: File;
+  private afs: AngularFirestore;
+
+  private profileService = new ProfileService(this.username, this.afs);
   constructor(username:string) { 
     this.username = username;
   }
@@ -24,9 +28,8 @@ export class ProfilePageComponent implements OnInit {
     let data = this.profileService.read()
     data.subscribe({
       next(data) {
-        this.bio = data.bio;
-        this.image = data.image;
-        this.mediums = data.mediums;
+        this.bio = data["bio"];
+        this.image = data["image"];
       }
     });
   }
--- communityCanvas/src/app/services/profile.service.ts
+++ communityCanvas/src/app/services/profile.service.ts
@@ -2,27 +2,31 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { DatabaseConnection } from 'src/app/classes/database-connection';
+import { Users } from 'src/app/interfaces/users';
 @Injectable({
   providedIn: 'root'
 })
-export class ProfileService extends DatabaseConnection {
+export class ProfileService extends DatabaseConnection implements Users{
+  username: string;
+  password: string;
+  bio: string;
+  image: File;
+  afs: AngularFirestore;
 
-  private collection:AngularFirestoreCollection<Profile>;
-  username:string;
-  protected afs:AngularFirestore;
+  private collection:AngularFirestoreCollection<Users>;
 
   store(ProfileService) {
     return;
   }
 
-  read():Observable<Profile> {
+  read():Observable<Users[]> {
     let data=this.collection.valueChanges();
     return data;
   }
 
-  constructor(username:string) { 
-    super(afs);
+  constructor(username:string, afs:AngularFirestore ) { 
+    super(afs);
     this.username = username;
-    this.collection = this.afs.collection('items', ref => ref.where("username", "==", username))<Profile>();
+    this.collection= this.afs.collection('items', ref => ref.where("username", "==", username));
   }
 }

0bff0f387dbea7c330e224d2dea6725d4ba2aa3b
Rebecca Parker <rfpparker@gmail.com>
2019-11-08 23:31:06 -0600

    Simple profile page setup

--- communityCanvas/src/app/app.module.ts
+++ communityCanvas/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule,
